b0ab6ed511fa8a11dabbac0cee9eccf82d898a3d
Nam Doan <nam.quang.doan@gmail.com>
2020-10-24 13:46:00 -0400

    Completion of tests with Lodash - Part 4a

---
+++ part4-blog/utils/list_helper.js
@@ -0,0 +1,60 @@
+var _ = require('lodash')
+
+const dummy = (blogs) => {
+	return 1
+}
+
+const totalLikes = (blogs) => {
+	return blogs.map(x => x.likes).reduce((total, x) => { return total + x })
+}
+
+const favoriteBlog = (blogs) => {
+	const result = blogs.reduce((prev, curr) => prev.likes>curr.likes ? prev:curr) 
+	delete result.__v
+	delete result._id
+	delete result.url
+	
+	return result
+}
+
+const mostBlogs = (blogs) => {
+	let most_author = ''
+	let num_blogs = 0
+	_.forEach(_.countBy(blogs, 'author'), (num, author) => {
+		if (num > num_blogs){
+			most_author = author
+			num_blogs = num
+		}
+	})
+	
+	return ({ 'author':most_author, 'blogs':num_blogs })
+}
+
+const mostLikes = (blogs) => {
+	let most_likes = 0
+	let author = ''
+	
+	_(blogs).groupBy('author').forEach(
+		(x, y) => {
+			total_likes = _.sumBy(x, 'likes')
+			if (total_likes > most_likes){
+				most_likes = total_likes
+				console.log('HERHERHERHERHERHER')
+				author = y
+			}
+		}
+	)
+	
+	return ({
+		'author':author,
+		'likes':most_likes
+	})
+}
+
+module.exports = {
+	dummy,
+	totalLikes,
+	favoriteBlog,
+	mostBlogs,
+	mostLikes
+}
