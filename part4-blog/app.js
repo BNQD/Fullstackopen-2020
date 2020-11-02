49034c717cabfcac2e960acd99e8b08e6e09a0c3
Nam Doan <nam.quang.doan@gmail.com>
2020-11-02 10:52:28 -0500

    Added token auth and user login/creation

--- part4-blog/app.js
+++ part4-blog/app.js
@@ -10,6 +10,8 @@ app.use(pretty({ query: 'pretty'}))
 const cors = require('cors')
 const blogRouter = require('./controllers/blog')
 const userRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
+
 const Blog = require('./models/blog')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -21,6 +23,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
