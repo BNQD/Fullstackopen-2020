35a8894e26da39a2db1fa38d53df5cf66b8cf614
Nam Doan <nam.quang.doan@gmail.com>
2020-11-13 10:06:51 -0500

    Added testing mongodb

--- part5-blog-backend/utils/config.js
+++ part5-blog-backend/utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 PORT = process.env.PORT
-MONGODB_URI = process.env.MONGODB_URI
+if(process.env.NODE_ENV === 'test'){
+	MONGODB_URI = process.env.MONGODB_URI_TESTING
+} else {
+	MONGODB_URI = process.env.MONGODB_URI
+}
 
 SECRET = process.env.SECRET
 
