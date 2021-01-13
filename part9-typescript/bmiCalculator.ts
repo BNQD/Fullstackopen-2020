3dd0bc185daa11881364062c12bf8f4df98d1bbc
Nam Doan <nam.quang.doan@gmail.com>
2021-01-13 15:28:54 -0500

    Completion part 9.4-9.5
    - Added HTTP GET endpoint for 'hello'
    - Added HTTP GET endpoint for BMI Calc using query string parameters

--- part9-typescript/bmiCalculator.ts
+++ part9-typescript/bmiCalculator.ts
@@ -1,5 +1,5 @@
 const calculateBmi = (weight: number, height: number) => {
-	const bmi = (weight/(height*height))
+	const bmi = (weight/(height/100*height/100))
 	if (bmi > 25) {
 		return 'overweight'
 	} else if (bmi > 18.5) {
@@ -12,3 +12,5 @@ const calculateBmi = (weight: number, height: number) => {
 const a: number = Number(process.argv[2])
 const b: number = Number(process.argv[3])
 console.log(calculateBmi(a, b))
+
+export { calculateBmi };
---
+++ part9-typescript/index.ts
@@ -0,0 +1,39 @@
+import express from 'express'
+const app = express();
+
+import { calculateBmi } from './bmiCalculator'
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+	
+	const weight = req.query.weight
+	const height = req.query.height
+	
+	let output = {}
+	
+	if (weight === undefined || height === undefined){
+		output = {
+			error: "missing inputs"
+		}
+	}
+	else {
+		const result = calculateBmi(+weight!, +height!)
+		output = {
+			weight,
+			height,
+			result
+		}
+	}
+	
+
+	res.send(output)
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
